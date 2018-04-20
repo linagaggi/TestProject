d662476d830ba31f2ee3aed0721492b9318b8584
LinaGaggi <l.gaggi@cogit.se>
2018-04-20 13:21:13 +0200

    added project

---
+++ TestProject/TestProject/Animals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public static class Animals
+    {
+        public const string Dog = "Dog";
+        public const string Cat = "Cat";
+        public const string Bird = "Bird";
+        public const string Fish = "Fish";
+    }
+}
---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your fav. animal?");
+            string animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case Animals.Fish:
+                    Console.WriteLine("You like Fish");
+                    break;
+                case Animals.Bird:
+                    Console.WriteLine("You like Birds");
+                    break;
+                case Animals.Cat:
+                    Console.WriteLine("You like Cats");
+                    break;
+                case Animals.Dog:
+                    Console.WriteLine("You like Dog");
+                    break;
+                default:
+                    Console.WriteLine("You don't like animals");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
